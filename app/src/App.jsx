d44eb1fcb7b8810fa2e53227d2989938e0493d6b
Jack Zhen <zwj1320@gmail.com>
2026-03-19 19:18:48 +1300

    auto focus, example about-me text,  Hero Section

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,9 +1,28 @@
-const App = () => {
+import { useRef, useState } from "react";
 
+const App = () => {
+  const [inputValue, setInputValue] = useState("");
+  const [showAboutMe, setShowAboutMe] = useState(false);
+
+  const inputRef = useRef(null);
+
+  const handleKeyDown = (e) => {
+    if (e.key === 'Enter') {
+      if (inputValue === 'about-me') {
+       setShowAboutMe(true);
+      }
+      console.log('execute command')
+      setInputValue('');
+    }
+  }
   return (
     // body
-    <div className='flex items-center justify-center min-h-dvh'>
-
+    <div className='flex flex-col items-center justify-center min-h-dvh'>
+      
+      <h1 className="font-geologica text-9xl">Hi,</h1>
+      <h2 className="font-geologica text-5xl">I'm Jack Zhen</h2>
+        
+      
       {/* cmd container */}
       <div className='border-white border w-4/6 '>
 
@@ -28,8 +47,21 @@ const App = () => {
           {/* command line */}
           <div className="flex">
             <span className="">C:\Users\zwj1320{">"}</span>
-            <input type="text" placeholder="enter your command here..."className="outline-none caret-white flex-1"></input>
+
+            <input type="text" placeholder="enter your command here..." value={inputValue} autoFocus
+            className="outline-none caret-white flex-1"
+            onChange={(e)=> setInputValue(e.target.value)}
+            onKeyDown={handleKeyDown}
+            ref={inputRef}
+            onBlur={()=>inputRef.current.focus()}>
+            </input>
           </div>
+
+          {/* About Me */}
+          {showAboutMe && (
+            <span>With a Bachelor of Science in Computer Science from The University of Auckland, I have developed a solid foundation in technology and hands-on experience across various roles. From designing and managing web applications as a Full Stack Developer Intern to optimizing SEO strategies as an IT & Web Administrator, my professional journey has equipped me with diverse skills. My leadership experience as Director of Event Planning for the Auckland University Chinese Student Society enhanced my ability to manage teams, coordinate logistics, and execute successful large-scale events.
+            </span>
+          )}
 
         </div>
         

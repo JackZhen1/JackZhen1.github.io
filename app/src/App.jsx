361da95e7e5a7fb239ac150e9c3a961f220c2c05
Jack Zhen <zwj1320@gmail.com>
2026-03-19 18:37:43 +1300

    basic command Prompt design

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,12 +1,42 @@
-import './App.css'
-
-function App() {
+const App = () => {
 
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
+    // body
+    <div className='flex items-center justify-center min-h-dvh'>
+
+      {/* cmd container */}
+      <div className='border-white border w-4/6 '>
+
+        {/* header */}
+        <div className='bg-gray-800 h-12 flex flex-col justify-end'>
+
+          {/* tab */}
+          <div className='bg-black w-fit ml-4 p-2 rounded-t-3xl'>
+            <span className='text-white px-2'>Command Prompt</span>
+          </div>
+        </div>
+
+        {/* window */}
+        <div className='p-2 font-bitcount text-xl' id=''>
+          <span className='text-2xl'>Commands: </span>
+          
+          <ul className="text-xl ml-2">
+            <li className="">{'> '}about-me</li>
+            <li>{'> '}projects</li>
+          </ul>
+          
+          {/* command line */}
+          <div className="flex">
+            <span className="">C:\Users\zwj1320{">"}</span>
+            <input type="text" placeholder="enter your command here..."className="outline-none caret-white flex-1"></input>
+          </div>
+
+        </div>
+        
+      </div>
+    </div>
+    
   )
 }
 
-export default App
+export default App;

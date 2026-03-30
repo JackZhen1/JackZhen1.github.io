e9adda659591125283c48ad9521a5930902ff856
Jack Zhen <zwj1320@gmail.com>
2026-03-31 01:19:55 +1300

    implemented basic projects component,

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,11 +1,13 @@
 import { useRef, useState} from "react";
 import useTypeWriter from "./hooks/useTypeWriter";
 import Social from "./components/social";
+import Projects from "./components/projects";
 
 const App = () => {
   const [inputValue, setInputValue] = useState("");
   const inputRef = useRef(null);
   const [showAboutMe, setShowAboutMe] = useState(false);
+  const [showProjects, setShowProjects] = useState(false);
 
   const titles = ["Full Stack Developer.", "Web Administrator.", "abcdwadwa."];
   const text = useTypeWriter(titles, 100, 50, 2000);
@@ -15,64 +17,73 @@ const App = () => {
     if (e.key === 'Enter') {
       if (inputValue === 'about-me') {
        setShowAboutMe(true);
+      } else if (inputValue === 'projects'){
+        setShowProjects(true);
       }
-      console.log('execute command')
       setInputValue('');
     }
   }
   return (
-    // body
     <div className="">
       <Social />
-      <div className='flex flex-col items-center justify-center min-h-dvh'>
+      <div className='flex flex-col'>
         
-        <h1 className="text-8xl">Hi,</h1>
-        <h2 className="text-5xl">I'm Jack Zhen</h2>
-        <h2 className="text-3xl">I'm a <span className="text-cyan-300">{text}</span>|</h2>
+        {/* body */}
+        <div className="flex flex-col items-center">
+          {/* hero text */}
+          <h1 className="text-8xl">Hi,</h1>
+          <h2 className="text-5xl">I'm Jack Zhen</h2>
+          <h2 className="text-3xl">I'm a <span className="text-cyan-300">{text}</span>|</h2>
 
-        {/* cmd container */}
-        <div className='border-white border w-4/6 '>
+          {/* cmd container */}
+          <div className='border-white border w-4/6 '>
 
-          {/* header */}
-          <div className='bg-gray-800 h-12 flex flex-col justify-end'>
+            {/* header */}
+            <div className='bg-gray-800 h-12 flex flex-col justify-end'>
 
-            {/* tab */}
-            <div className='bg-black w-fit ml-4 p-2 rounded-t-3xl'>
-              <span className='text-white px-2'>Command Prompt</span>
-            </div>
-          </div>
-
-          {/* window */}
-          <div className='p-2 font-bitcount text-xl' id=''>
-            <span className='text-2xl'>Commands: </span>
-            
-            <ul className="text-xl ml-2">
-              <li className="">{'> '}about-me</li>
-              <li>{'> '}projects</li>
-            </ul>
-            
-            {/* command line */}
-            <div className="flex">
-              <span className="">C:\Users\zwj1320{">"}</span>
-
-              <input type="text" placeholder="enter your command here..." value={inputValue} autoFocus
-              className="outline-none caret-white flex-1"
-              onChange={(e)=> setInputValue(e.target.value)}
-              onKeyDown={handleKeyDown}
-              ref={inputRef}
-              onBlur={()=>inputRef.current.focus()}>
-              </input>
+              {/* tab */}
+              <div className='bg-black w-fit ml-4 p-2 rounded-t-3xl'>
+                <span className='text-white px-2'>Command Prompt</span>
+              </div>
             </div>
 
-            {/* About Me */}
-            {showAboutMe && (
-              <span>With a Bachelor of Science in Computer Science from The University of Auckland, I have developed a solid foundation in technology and hands-on experience across various roles. From designing and managing web applications as a Full Stack Developer Intern to optimizing SEO strategies as an IT & Web Administrator, my professional journey has equipped me with diverse skills. My leadership experience as Director of Event Planning for the Auckland University Chinese Student Society enhanced my ability to manage teams, coordinate logistics, and execute successful large-scale events.
-              </span>
-            )}
+            {/* window */}
+            <div className='p-2 font-bitcount text-xl h-150'>
+              <span className='text-2xl'>Commands: </span>
+              
+              <ul className="text-xl ml-2">
+                <li className="">{'> '}about-me</li>
+                <li>{'> '}projects</li>
+              </ul>
+              
+              {/* command line */}
+              <div className="flex">
+                <span className="">C:\Users\zwj1320{">"}</span>
 
+                <input type="text" placeholder="enter your command here..." value={inputValue} autoFocus
+                className="outline-none caret-white flex-1"
+                onChange={(e)=> setInputValue(e.target.value)}
+                onKeyDown={handleKeyDown}
+                ref={inputRef}
+                onBlur={()=>inputRef.current.focus()}>
+                </input>
+              </div>
+
+              {/* About Me */}
+              {showAboutMe && (
+                <span>With a Bachelor of Science in Computer Science from The University of Auckland, I have developed a solid foundation in technology and hands-on experience across various roles. From designing and managing web applications as a Full Stack Developer Intern to optimizing SEO strategies as an IT & Web Administrator, my professional journey has equipped me with diverse skills. My leadership experience as Director of Event Planning for the Auckland University Chinese Student Society enhanced my ability to manage teams, coordinate logistics, and execute successful large-scale events.
+                </span>
+              )}
+
+              {showProjects && <Projects />}
+
+            </div>
+            
           </div>
-          
         </div>
+        
+
+        
       </div>
 
     </div>
---
+++ app/src/components/projects.jsx
@@ -0,0 +1,58 @@
+import { useRef, useEffect } from "react";
+
+const images = [
+    'https://images.unsplash.com/photo-1518770660439-4636190af475?w=500',
+    'https://images.unsplash.com/photo-1526374965328-7f61d4dc18c5?w=500',
+    'https://images.unsplash.com/photo-1550745165-9bc0b252726f?w=500',
+    'https://images.unsplash.com/photo-1581091226825-a6a2a5aee158?w=500',
+    'https://images.unsplash.com/photo-1563986768609-322da13575f3?w=500',
+    'https://images.unsplash.com/photo-1531297484001-80022131f5a1?w=500',
+];
+
+const Projects = () => {
+    const imgLength = images.length
+
+    const ringRef = useRef(null);
+    const rotation = useRef(0);
+    const animationFrame = useRef(null);
+
+    const updateAnimation = () => {
+        const constSpeed = 0.4
+        rotation.current += constSpeed
+
+        if (ringRef.current) {
+            ringRef.current.style.transform = `rotateY(${rotation.current}deg)`;
+        }
+
+        animationFrame.current = requestAnimationFrame(updateAnimation);
+    }
+
+    useEffect(() => {
+    animationFrame.current = requestAnimationFrame(updateAnimation);
+
+    return () => cancelAnimationFrame(animationFrame.current);
+    }, []);
+    const handleWheel = (e) => {
+
+    }
+    return (
+        <div className="w-full h-125 flex items-center justify-center perspective-[7000px]" onWheel={handleWheel}>
+            <div className="relative w-50 h-75" style={{ transformStyle: 'preserve-3d' }} ref={ringRef}>
+
+                {images.map((img, index) => {
+                    const angle = 180/imgLength * index - 90
+                    return (
+                        <div key={index} className="absolute top-0 left-0 w-full h-full" 
+                        style={{transform: `rotateY(${angle}deg) translateZ(${1200/Math.PI}px)`}}>
+                            <img src={img} className="w-full h-full object-cover" />
+                        </div>
+                    )
+                })}
+
+            </div>
+        </div>
+    )
+    
+};
+
+export default Projects;
